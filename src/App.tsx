62de56e0d67a44db4fe7906b3946e3d3e01efa8e
Michael Matich <matich.mike@gmail.com>
2021-05-04 18:56:08 -0400

    cart and cartitem components scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import {useState} from 'react';
 import {useQuery} from 'react-query';
 
-import Item from './Item/Item'
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 
-import {Wrapper} from './App.styles';
+import {Wrapper, StyledButton} from './App.styles';
 
 export type CartItemType = {
   id: number;
@@ -24,12 +23,17 @@ export type CartItemType = {
 const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
   const {data, isLoading, error} = useQuery<CartItemType[]>(
     'products', 
     getProducts
   );
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+    items.reduce((acc: number, item) => acc + item.amount, 0);
+  ;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -40,6 +44,14 @@ const App = () => {
     
   return (
     <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrappr = styled.aside`
+  font-family: Arial, sans-serif;
+  width: 500px;
+  padding: 20px;
+`
