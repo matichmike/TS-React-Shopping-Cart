578bf2878f83cfeb5c5ed0b8d869145ec9711530
Michael Matich <matich.mike@gmail.com>
2021-05-04 22:33:35 -0400

    adding items to cart works

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import {useQuery} from 'react-query';
 
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -35,7 +36,20 @@ const App = () => {
     items.reduce((acc: number, item) => acc + item.amount, 0);
   ;
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      const isItemInCart =  prev.find(item => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map(item => 
+          item.id === clickedItem.id
+          ? {...item, amount:item.amount + 1}
+          : item
+        )
+      }
+      return [...prev, {...clickedItem, amount: 1}];
+    })
+  };
 
   const handleRemoveFromCart = () => null;
 
@@ -45,7 +59,7 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart cartItems={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color='error'>
--- src/Cart/Cart.styles.ts
+++ src/Cart/Cart.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrappr = styled.aside`
+export const Wrapper = styled.aside`
   font-family: Arial, sans-serif;
   width: 500px;
   padding: 20px;
